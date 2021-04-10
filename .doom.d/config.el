4f55181b569c822af485826681dc6763823eca5c
KoreyMoffett <korey.moffett87@gmail.com>
2021-04-09 21:45:56 -0500

    I dont remember if im honest

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,9 +1,10 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Personal Info
 (setq user-full-name "Korey Moffett"
       user-mail-address "kaosfang@tutanota.com")
-;; Full Screen
+
+(defvar +doom-dashboard-banner-file "~/.doom.d/gnu.jpeg")
+(defvar +doom-dashboard-banner-dir "~.doom.d")
+
 (if (eq initial-window-system 'x)
     (toggle-frame-maximized)
   (toggle-frame-fullscreen))
@@ -16,16 +17,12 @@
 
 (setq global-prettify-symbols-mode t)
 
-;; Theme
 (setq doom-theme 'doom-one)
 
-;Org
 (setq org-directory "~/org/")
 
-;; Number Line
 (setq display-line-numbers-type 'relative)
 
-;; Keybinds
 (setq-default evil-escape-key-sequence "ii")
 (setq-default evil-escape-delay 0.2)
 
@@ -38,7 +35,6 @@
       (:prefix ("m" . "matodon-toot")
        :desc "Mastodon Toot" "m" 'mastodon-toot))
 
-;; Elfeed
 (require 'elfeed-goodies)
 (elfeed-goodies/setup)
 (setq elfeed-goodies/entry-pane-size 0.5)
@@ -54,16 +50,14 @@
                      ("https://static.fsf.org/fsforg/rss/blogs.xml" Blog FOSS)
                      ("https://static.fsf.org/fsforg/rss/news.xml" News FOSS))))
 
-;; Mastodon
 (after! mastodon
   (setq mastodon-instance-url "https://fosstodon.org"))
 
-;; Org Bullet
 (use-package org-bullets
   :ensure t
   :config
   (add-hook 'org-mode-hook (lambda  () (org-bullets-mode 1))))
-;; Org Superstart
+
 (use-package org-superstar
   :ensure t
   :config
--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -5,10 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(elfeed-feeds '("https://koreymoffett.com/rss.xml"))
  '(package-selected-packages
-   '(elfeed-goodies mastodon emms dired-rainbow org-superstar org-super-agenda org-fancy-priorities all-the-icons emojify org-bullets org-pretty-tags org-plus-contrib)))
+   '(elfeed-goodies mastodon dired-rainbow org-superstar org-super-agenda org-fancy-priorities all-the-icons emojify org-bullets org-pretty-tags org-plus-contrib)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-keyword-face ((t (:slant italic)))))
